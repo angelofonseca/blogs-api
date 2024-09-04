d1c11388c07220fc80706b85a3a27ddc104a84dc
angelofonseca <angeloniemeyer97@gmail.com>
2024-09-04 19:25:42 -0300

    Ajuste no middleware de autorização

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,10 +2,10 @@ const { verify } = require('../utils/auth');
 
 module.exports = (req, res, next) => {
   const bearerToken = req.headers.authorization;
-  const token = bearerToken.split(' ')[1];
 
-  if (!token) return res.status(401).json({ message: 'Token not found' });
+  if (!bearerToken) return res.status(401).json({ message: 'Token not found' });
   try {
+    const token = bearerToken.split(' ')[1];
     const user = verify(token);
 
     req.locals = { user };
