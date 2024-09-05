2bf57ca5f68adfae7fc1fcd2b79ed8845d0ffc06
angelofonseca <angeloniemeyer97@gmail.com>
2024-09-04 22:27:01 -0300

    Cria a model de PostCategory e relaciona com a model Category e BlogPost

---
+++ src/models/PostCategory.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      foreignKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      foreignKey: true,
+    },
+  },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'posts_categories',
+    });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blog_posts',
+      foreignKey: 'categoryId',
+      through: PostCategory,
+      otherKey: 'postId',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'blog_posts',
+      foreignKey: 'postId',
+      through: PostCategory,
+      otherKey: 'categoryId',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    });
+  }
+
+  return PostCategory;
+}
