c6662a692175e9cd3429924d4237f9dda4bedcdf
angelofonseca <angeloniemeyer97@gmail.com>
2024-09-06 10:01:51 -0300

    Ajuste na função que cria novo post Valida corretamente as categorias do novo post

--- src/services/BlogPost.service.js
+++ src/services/BlogPost.service.js
@@ -8,7 +8,7 @@ const create = async (post, email) => {
   if (error) return error;
   const { id: userId } = await User.findOne({ where: { email } });
   const categories = await Category.findAll();
-  const result = categories.every(({ id: categoryId }) => post.categoryIds.includes(categoryId));
+  const result = post.categoryIds.every((catId) => categories.map(({ id }) => id).includes(catId));
   if (!result) return { status: 400, data: { message: 'one or more "categoryIds" not found' } };
   const newPost = await BlogPost.create(
     { userId, ...post },
